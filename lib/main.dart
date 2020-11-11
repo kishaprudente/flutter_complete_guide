3a790d08e367998f5aabdb47e261d9b0634ce4d5
Kisha Prudente <kisha.prudente@gmail.com>
2020-11-11 00:28:33 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = ['What\'s your name?', 'What\'s your favourite color?'];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Kisha'),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
